6ba46528fb161e12c132d595c86cea6b81966711
ZeizerLaite <gabito.alf.sal@gmail.com>
2026-03-05 12:58:16 -0600

    added a little bit of skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,38 @@
+#include <iostream>
+#include <fstream>
 #include <raylib-cpp.hpp>
+#include <cstdint>
+namespace entities{
+class Entity 
+{
+    public:
+    uint8_t health = 100;
+    //Add the missing arguments and also make the other extended classes (player, per-type enemy)
+    private:
+
+};
+}
+namespace fstream{
+void loadSprites()
+{
+    //Add the image loading from fstream maybe?
+};
+}
+
+namespace levels
+{
+    class level
+    {
+        public:
+
+        private:
+        //find datatype for images to have background
+        uint8_t surviveTimePerWave;
+        char difficulty;
+        uint8_t waves;
+        uint8_t increasingAmountOfEnemies;
+    };
+}
 
 int main() {
     
